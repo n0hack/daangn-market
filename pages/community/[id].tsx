3531640ee7935f11509f951a34b7394341cd6c00
n0hack <nohack-@naver.com>
2022-07-20 01:06:30 +0900

    [fix] placeholder 수정

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -73,7 +73,7 @@ export default function CommunityPostDetail() {
         <textarea
           className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500 "
           rows={4}
-          placeholder="질문을 작성해주세요!"
+          placeholder="댓글을 작성해주세요!"
         />
         <button className="mt-2 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none ">
           작성하기
